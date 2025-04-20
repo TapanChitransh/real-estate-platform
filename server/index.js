c300cb1566453b9f7e814244d8ee025101e7ef1a
Tapan Chitransh <kingkaajooba@gmail.com>
2025-04-20 11:30:06 +0530

    Added Mongo Db connection to fetch data

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const app = require('./src/app');
-//const connectDB = require('./src/config/db');
+const connectDB = require('./src/config/db');
 const PORT = process.env.PORT || 5000;
 
-
-//console.log(connectDB());
+console.log("Db is connected!!!");
+console.log(connectDB());
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
--- server/src/services/propertyServices.js
+++ server/src/services/propertyServices.js
@@ -3,6 +3,5 @@ const Property = require('../models/propertiesModel');
 exports.getAllProperties =async () =>{
     const data = await Property.find();
     console.log("property api is hit at service level");
-    
     return data;
 }
